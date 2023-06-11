8e2dc3d262f6fb97179ea480e1a5fa0d66e587bd
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-06-11 22:17:08 +0000

    remove GCC

--- plebbit-build.sh
+++ plebbit-build.sh
@@ -7,17 +7,17 @@ rm -fr dist
 mkdir -p dist
 
 # build linux amd64
-make build GOOS=linux GOARCH=amd64
+make build GOOS=linux GOARCH=amd64 CGO_ENABLED=0
 mv cmd/ipfs/ipfs dist/ipfs-linux-amd64
 
 # build linux arm64
-make build GOOS=linux GOARCH=arm64
+make build GOOS=linux GOARCH=arm64 CGO_ENABLED=0
 mv cmd/ipfs/ipfs dist/ipfs-linux-arm64
 
 # build mac amd64
-make build GOOS=darwin GOARCH=amd64
+make build GOOS=darwin GOARCH=amd64 CGO_ENABLED=0
 mv cmd/ipfs/ipfs dist/ipfs-darwin-amd64
 
 # build windows amd64
-make build GOOS=windows GOARCH=amd64
+make build GOOS=windows GOARCH=amd64 CGO_ENABLED=0
 mv cmd/ipfs/ipfs dist/ipfs-windows-amd64
